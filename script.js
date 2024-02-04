d9a8c4eec518dd126bc7c63b559e8f63f4df0254
PITT1 <petroleo.plta.97@gmail.com>
2024-02-04 00:55:20 -0400

    pequeña prueba

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var pokemonNames = [];
 
 async function fetchData(){
     try {
-        for (let i = 1; i < 1026; i++) {
+        for (let i = 1; i < 500; i++) {
             const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
             const data = await response.json();
 
